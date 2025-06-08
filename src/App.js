e268ba5976bf941c0aaae972cca022ed2228f22a
agent007DXB <cyberjunkeez.org@gmail.com>
2025-06-08 15:45:49 +0530

    Update App.js
    proected routed add to app.js if doesnt work switch back to old file from git final

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
+// src/App.js
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import UserProfile from "./pages/UserProfile";
+import Profile from "./pages/Profile";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Disclaimer from "./pages/Disclaimer";
 import AdminDashboard from "./pages/AdminDashboard";
-import Deposit from "./pages/Deposit";
-import Investments from "./pages/Investments";
 import KYCUpload from "./components/KYCUpload";
-import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
 import WithdrawRequest from "./pages/WithdrawRequest";
-import Profile from "./pages/Profile"; // ✅ make sure it's imported
-
-
-
+import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedAdminRoute from "./components/ProtectedAdminRoute"; // ✅ Add this
 
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-
-            <Route path="/dashboard" element={
-              <PrivateRoute>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
                 <Dashboard />
-              </PrivateRoute>
-            } />
-            <Route path="/deposit" element={
-              <PrivateRoute>
-                <Deposit />
-              </PrivateRoute>
-            } />
-            <Route path="/investments" element={
-              <PrivateRoute>
-                <Investments />
-              </PrivateRoute>
-            } />
-            <Route path="/profile" element={
-              <PrivateRoute>
-                <UserProfile />
-              </PrivateRoute>
-            } />
-
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/disclaimer" element={<Disclaimer />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/kyc-upload" element={<KYCUpload />} />
-            <Route path="/withdraw" element={<WithdrawRequest />} />
-            import Profile from "./pages/Profile"; // ✅ make sure it's imported
-
-<Route path="/profile" element={<Profile />} />
-
-
-          </Routes>
-        </main>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/withdraw"
+            element={
+              <ProtectedRoute>
+                <WithdrawRequest />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/kyc"
+            element={
+              <ProtectedRoute>
+                <KYCUpload />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedAdminRoute>
+                <AdminDashboard />
+              </ProtectedAdminRoute>
+            }
+          />
+        </Routes>
         <Footer />
       </div>
     </Router>
