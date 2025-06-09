2e51f9630a0af4e5c4c2b4e4327edcbff2e2e6cf
agent007DXB <cyberjunkeez.org@gmail.com>
2025-06-09 18:25:44 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
       <div className="max-w-xl mx-auto p-6 mt-10 text-gray-700">
         <h1 className="text-2xl font-bold mb-4 text-center">Contact Us</h1>
         <p>Email: support@barakainvestment.com</p>
-        <p>Phone: +91-123-456-7890</p>
+        <p>Phone: +1-206-414-1863</p>
         <p>Address: Baraka HQ, Business Tower, Mumbai, India</p>
       </div>
     );
